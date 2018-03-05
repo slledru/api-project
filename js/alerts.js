627a8900348bdda6964a2628e82e5ee1f890a260
slledru <slledru@msn.com>
2018-03-04 17:26:58 -0700

    place holder for screens

--- js/alerts.js
+++ js/alerts.js
@@ -3,9 +3,5 @@ $(document).ready(() => {
   //   appKey: 'd2009af5b68f488b',
   //   location: 'CO/Boulder'
   // }
-  //api.getCurrentCondition(appKey, location, dom.drawCurrentCondition)
   api.getWeatherAlerts(supplement.appKey, supplement.location, dom.drawWeatherAlerts)
-  //api.getOneDayForecast(appKey, location, dom.drawOneDayForecast)
-  //api.getFiveDayForecast(appKey, location, dom.drawFiveDayForecast)
-
 })
---
+++ js/options.js
@@ -0,0 +1,6 @@
+$(document).ready(() => {
+  // const supplement = {
+  //   appKey: 'd2009af5b68f488b',
+  //   location: 'CO/Boulder'
+  // }
+})
