310e10be093e6d8897e57cc89af066ef557ba5da
slledru <slledru@msn.com>
2018-03-06 14:22:02 -0700

    reformatted

--- public/js/app.js
+++ public/js/app.js
@@ -177,54 +177,73 @@ $(document).ready(() => {
     }
   }
   function drawPlannerChart() {
-    var ctx = document.getElementById("chart").getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-            datasets: [{
-                label: 'High Temperature (°F)',
-                data: [32, 29, 50, 54, 60, 67, 80, 95, 78, 65, 40, 35],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)',
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
+    let ctx = document.getElementById("chart").getContext('2d');
+    let myChart = new Chart(ctx, {
+      type: 'bar',
+      data:
+      {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        datasets:
+        [
+          {
+            label: 'High Temperature (°F)',
+            data: [32, 29, 50, 54, 60, 67, 80, 95, 78, 65, 40, 35],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            // backgroundColor: [
+            //     'rgba(255, 99, 132, 0.2)',
+            //     'rgba(54, 162, 235, 0.2)',
+            //     'rgba(255, 206, 86, 0.2)',
+            //     'rgba(75, 192, 192, 0.2)',
+            //     'rgba(153, 102, 255, 0.2)',
+            //     'rgba(255, 159, 64, 0.2)',
+            //     'rgba(255, 99, 132, 0.2)',
+            //     'rgba(54, 162, 235, 0.2)',
+            //     'rgba(255, 206, 86, 0.2)',
+            //     'rgba(75, 192, 192, 0.2)',
+            //     'rgba(153, 102, 255, 0.2)',
+            //     'rgba(255, 159, 64, 0.2)'
+            // ],
+            // borderColor: [
+            //   'rgba(255,99,132,1)',
+            //   'rgba(54, 162, 235, 1)',
+            //   'rgba(255, 206, 86, 1)',
+            //   'rgba(75, 192, 192, 1)',
+            //   'rgba(153, 102, 255, 1)',
+            //   'rgba(255, 159, 64, 1)',
+            //   'rgba(255,99,132,1)',
+            //   'rgba(54, 162, 235, 1)',
+            //   'rgba(255, 206, 86, 1)',
+            //   'rgba(75, 192, 192, 1)',
+            //   'rgba(153, 102, 255, 1)',
+            //   'rgba(255, 159, 64, 1)'
+            // ],
+            borderWidth: 1
+          }
+        ]
+      },
+      options:
+      {
+        scales:
+        {
+          yAxes:
+          [
+            {
+              stacked: true,
+              ticks:
+              {
+                beginAtZero:true
+              }
             }
+          ],
+          xAxes:
+          [
+            {
+              stacked: true
+            }
+          ]
         }
-    });
+      }
+    })
   }
 })
