a4518993a0ddb7293ed07af232816a16ce80a7bc
slledru <slledru@msn.com>
2018-03-06 20:04:49 -0700

    moved sample data to read from unit test (#9)

--- public/test/tests.js
+++ public/test/tests.js
@@ -1,5 +1,19 @@
 const expect = chai.expect
 
+const sampleData = {
+  alertCedarvale: JSON.parse(alertCedarvale),
+  currentWeather: JSON.parse(currentWeather),
+  historicalSummary: JSON.parse(historicalSummary),
+  historicalWeather: JSON.parse(historicalWeather),
+  oneDay: JSON.parse(oneDay),
+  tenDay: JSON.parse(tenDay)
+}
+
 describe('API', function() {
-  
+  console.log(sampleData.alertCedarvale);
+  console.log(sampleData.currentWeather);
+  console.log(sampleData.historicalSummary);
+  console.log(sampleData.historicalWeather);
+  console.log(sampleData.oneDay);
+  console.log(sampleData.tenDay);
 })
